b1866e85b5a1f6a2a9ba65f52d264ab5e2d9df1d
alisaosx <aliasosx@hotmail.com>
2019-03-27 15:43:19 +0700

    update food

--- functions/routers/foods.js
+++ functions/routers/foods.js
@@ -24,18 +24,40 @@ module.exports = server => {
     });
     server.post('/foods', async (req, res, next) => {
         const foods = await Food.create(req.body).then(async (x) => {
-
-            req.body.subfoods.forEach(async (fd) => {
-                console.log(fd.foodName);
+            // check subfood come
+            if (req.body.subfoods != null) {
+                req.body.subfoods.forEach(async (fd) => {
+                    console.log('###################### Containe subfood with id ' + fd.id + ' ######################');
+                    const foodtranx = await FoodTranx.create({
+                        foodId: x.id,
+                        subfoodId: fd.id,
+                        cost: fd.cost,
+                        price: fd.price,
+                        currencyId: req.body.currencyId,
+                        updateById: req.body.updatedBy,
+                    }).then((y) => {
+                        console.log('Food added ' + x.id);
+                    }).catch(async (err) => {
+                        console.log(err);
+                        const ftx = await Food.destroy({
+                            where: {
+                                id: x.id
+                            }
+                        }).then((z) => {
+                            console.log('###################### Food rolled back ######################');
+                        });
+                    });
+                });
+            } else {
+                // Not subfood
                 const foodtranx = await FoodTranx.create({
                     foodId: x.id,
-                    subfoodId: fd.subfoodId,
-                    cost: fd.cost,
-                    price: fd.price,
+                    cost: req.body.cost,
+                    price: req.body.price,
                     currencyId: req.body.currencyId,
                     updateById: req.body.updatedBy,
                 }).then((y) => {
-                    console.log('Food added ' + x.id);
+                    console.log('###################### Food added ' + x.id + ' ######################');
                 }).catch(async (err) => {
                     console.log(err);
                     const ftx = await Food.destroy({
@@ -46,13 +68,12 @@ module.exports = server => {
                         console.log('###################### Food rolled back ######################');
                     });
                 });
-            });
+            }
             res.send({ status: 'success' });
             next();
-
         }).catch((err) => {
             console.log(err);
-            res.send({ status: 'err' });
+            res.send({ status: 'error' });
             next();
         });
     });
@@ -115,10 +136,28 @@ module.exports = server => {
         next();
     });
 
-    server.get('/subfoods', async (req, res, next) => {
-        const subfoods = await Subfood.findAll();
-        res.send(subfoods);
-        next();
+    server.get('/foodsDisplay', async (req, res, next) => {
+        const foods = await sequenlize.query("select fd.id,fd.food_name,fd.food_name_en,fd.food_photo ,sf.subFoodName,ft.cost , ft.price, ft2.foodTypeName , cr.currency_name, ks.kitchenName , fd.isParent from foods fd, foodTranx ft, subfoods sf, foodType ft2 , currencies cr , kitchens ks where ks.id = fd.kitchenId and fd.id = ft.foodId and ft.subFoodId = sf.id and fd.foodTypeId = ft2.id and ft.currencyId = cr.id group by fd.food_name union  all select fd.id,fd.food_name,fd.food_name_en,fd.food_photo,'',ft.cost , ft.price , fT3.foodTypeName, cr.currency_name , ks.kitchenName, fd.isParent from foods fd, foodTranx ft , foodType fT3, currencies cr , kitchens ks where ks.id = fd.kitchenId and fd.id = ft.foodId and ft.subFoodId is null and fT3.id = fd.foodTypeId and ft.currencyId = cr.id group by fd.food_name", { type: sequelize.QueryTypes.SELECT })
+            .then((foods) => {
+                res.send(foods);
+                next();
+            }).catch((err) => {
+                console.log(err);
+                res.send({ status: 'error' });
+                next();
+            });
     });
-
+    server.get('/subfoodByFoodId/:id', async (req, res, next) => {
+        const subfoods = await Subfood.findAll({
+            where: {
+                foodId: req.params.id
+            }
+        }).then((resp) => {
+            res.send(resp);
+            next();
+        }).catch((err) => {
+            res.send({ status: 'error' });
+            next();
+        });
+    });
 };
